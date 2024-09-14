d5fbd3931ce19268449a2ecfee3b4b3a735c6fd1
Blushyes <Blushyes@gmail.com>
2024-09-15 01:38:56 +0800

    :bug: 修复类型问题

--- executor/__init__.py
+++ executor/__init__.py
@@ -16,13 +16,13 @@ class ScriptStep:
 def execute(context: ScriptInfo, script: list[ScriptStep]):
     i = 0
     while i < len(script):
-        cmd: ScriptStep = script[i]
+        cmd: ScriptStep = script[int(i)]
         cmd.executor.execute(context, cmd.arg)
 
         if Cosmic.pause_executor:  # 如果用户选择暂停执行，则退出循环
             return None
 
         if cmd.jump_to is not None:
-            i = cmd.jump_to
+            i = int(cmd.jump_to)
         else:
             i += 1
